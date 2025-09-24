ac2aafe251450b794f5eb5417b29df75241358d6
mstefancich <57668115+mstefancich@users.noreply.github.com>
2025-09-24 14:49:24 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -3,6 +3,7 @@ self.addEventListener("install", e => {
     e.waitUntil(
         caches.open("static").then(cache => {
             return cache.addAll([
+              '/',
               '/index.html',
               '/app.js',
               '/manifest.webmanifest',
