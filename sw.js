7bada9a30eed3a3adb225bcdf91c4679104a7b18
mstefancich <57668115+mstefancich@users.noreply.github.com>
2025-09-24 14:47:47 +0200

    Update sw.js
    added caching

--- sw.js
+++ sw.js
@@ -1,3 +1,16 @@
 self.addEventListener("install", e => {
-    console.log("Installing sw");
+    console.log("Caching resources..");
+    e.waitUntil(
+        caches.open("static").then(cache => {
+            return cache.addAll([
+              '/index.html',
+              '/app.js',
+              '/manifest.webmanifest',
+              '/icons/icon-192.png',
+        ]);
+        })
+    );
+    console.log("Resources in cache. Done");
+})
+
 });
